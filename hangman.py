de5b5bc45b3af5611e719770926205d954e1c732
Garvit-Mehra <114169456+Garvit-Mehra@users.noreply.github.com>
2024-08-05 12:08:18 +0530

    Updating hangman.py

--- hangman.py
+++ hangman.py
@@ -4,11 +4,11 @@ from termcolor import cprint
 
 
 def rules():
-    cprint("\t\tHANGMAN", attrs=["bold"], color="blue")
+    cprint("\t\t\t\t\t\tHANGMAN", attrs=["bold"], color="blue")
     cprint(
         "Rules: \n1. Choose the amount of guesses you requires ranging from 1 to 11.\n2. Enter any letter "
         "into the input space.\n3. If the letter is part of the word the blank spaces will be filled\n4. If the "
-        "letter is not part of the word, one try shall be deducted.\n5.The game ends if either your tries are over "
+        "letter is not part of the word, one try shall be deducted.\n5. The game ends if either your tries are over "
         "or the word I guessed correctly.\n6. Remember to have fun!\n",
         color="blue",
     )
@@ -21,14 +21,16 @@ def get_word(doc):
         return lines[i].rstrip()
 
 
-def tries():
+def get_tries():
     while True:
         try:
             i = int(input("Enter number of tries you would like: "))
             if i >= 12:
                 print("Too many tries requested!")
                 pass
-            elif 1 <= i <= 11:
+            elif i < 1:
+                print("Too little tries requested!")
+            else:
                 break
         except ValueError:
             pass
@@ -49,37 +51,43 @@ def get_input():
 def game():
     word = list(get_word("words.csv").rstrip())
     answer = "".join(word)
-    tri = tries()
+    tries = get_tries()
     guesses = []
+    guess_list = []
 
     for i in range(len(word)):
         guesses.append("_")
 
     while True:
-        print(f"\nYou have {tri} tries left")
-        print(" ".join(guesses))
+        print(f"\nYou have {tries} tries left")
+        print("Word:", " ".join(guesses))
         letter = get_input()
 
-        if letter in word:
-            for x in range(len(list(answer))):
-                if list(answer)[x] == letter:
-                    guesses[x] = letter
-                else:
-                    pass
-            word.remove(letter)
+        if letter not in guess_list:
+            if letter in word:
+                for x in range(len(list(answer))):
+                    if list(answer)[x] == letter:
+                        guesses[x] = letter
+                    else:
+                        pass
+                word.remove(letter)
+                guess_list.append(letter)
 
-        if tri != 0 and "".join(guesses) == answer:
-            print("\n" + f"{' '.join(list(answer))}")
-            cprint("You Win!", "green")
-            sys.exit(0)
-        elif tri == 0:
-            cprint(f"The word was {answer}\nYou Lose!", "red")
-            sys.exit(0)
-        if letter not in list(answer):
-            tri -= 1
-            if tri == 0:
-                cprint(f"\nThe word was {answer}\nYou Lose!", "red")
+            if tries != 0 and "".join(guesses) == answer:
+                print("\n" + f"{' '.join(list(answer))}")
+                cprint("You Win!", "green")
                 sys.exit(0)
+            elif tries == 0:
+                cprint(f"The word was {answer}\nYou Lose!", "red")
+                sys.exit(0)
+            if letter not in list(answer):
+                tries -= 1
+                guess_list.append(letter)
+                if tries == 0:
+                    cprint(f"\nThe word was {answer}\nYou Lose!", "red")
+                    sys.exit(0)
+        else:
+            print("\nLetter already used!")
 
 
 def main():
